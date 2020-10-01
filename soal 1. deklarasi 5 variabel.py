bf2bd13ee048b450835d1a33149c5ff75bad174c
izzul741 <72200560+izzul741@users.noreply.github.com>
2020-10-01 18:12:27 +0700

    Add files via upload

---
+++ soal 1. deklarasi 5 variabel.py
@@ -0,0 +1,14 @@
+"""
+Created on Oct 01 2020
+
+@author: 190411100031/Nur Mu'izzul Qodir
+"""
+
+bil1 = 21
+bil2 = 33
+bil3 = 42
+bil4 = 64
+bil5 = 57
+
+jumlah = (bil1+bil2+bil3+bil4+bil5)/5
+print("Hasil Perhitungan", jumlah)
