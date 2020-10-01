6e6ddb28caaf218d5f4fd525c05b0a2b5f7a3884
izzul741 <72200560+izzul741@users.noreply.github.com>
2020-10-01 18:18:02 +0700

    Add files via upload

---
+++ soal 2. menghitung luas dan keliling.py
@@ -0,0 +1,16 @@
+"""
+Created on Oct 01 2020
+
+@author: 190411100031/Nur Mu'izzul Qodir
+"""
+
+print("Program menghitung luas dan keliling bangun datar")
+
+p = int(input("Masukan Panjang = "))
+l = int(input("Masukan Luas    = "))
+
+luas = p*l
+kel = 2*(p+l)
+
+print("Luas Persegi Panjang Adalah = ", luas)
+print("Keliling Persegi Panjang Adalah = ", kel)
